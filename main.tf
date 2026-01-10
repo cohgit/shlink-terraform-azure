2f7c45db1482ef1dda81842099146b8165b6367a
César Ogalde H <cesar.ogalde@gmail.com>
2026-01-09 23:48:08 -0300

    Fix: Allow insecure HTTP (CNAME restricted by DNS provider)

--- main.tf
+++ main.tf
@@ -216,6 +216,7 @@ resource "azurerm_container_app" "shlink_web" {
 
   ingress {
     external_enabled = true
+    allow_insecure   = true
     target_port      = 8080
     traffic_weight {
       percentage      = 100
