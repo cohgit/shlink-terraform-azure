a28f2cd35a566590aa5c68d6142ab4d6110ecdc5
César Ogalde H <cesar.ogalde@gmail.com>
2026-01-09 22:31:26 -0300

    Fix: Append azurerm_container_app_custom_domain resource

--- main.tf
+++ main.tf
@@ -223,3 +223,10 @@ resource "azurerm_container_app" "shlink_web" {
     }
   }
 }
+
+# Custom Domain for Shlink
+resource "azurerm_container_app_custom_domain" "shlink" {
+  name                      = var.shlink_default_domain
+  container_app_id          = azurerm_container_app.shlink.id
+  certificate_binding_type  = "Managed"
+}
